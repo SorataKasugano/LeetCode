e26642eba8818f24660dc4422fd80d012341df3b
Sorata Kasugano <884244164@qq.com>
2018-03-22 09:31:48 +0800

    217. Contains Duplicate

--- Algorithm/ContainsDuplicate.h
+++ Algorithm/ContainsDuplicate.h
@@ -6,10 +6,15 @@ return true if any value appears at least twice in the array, and it should retu
 every element is distinct. */
 
 #include <vector>
+#include <unordered_set>
 #include <unordered_map>
 using namespace std;
 
 bool containsDuplicate(vector<int>& nums) {
+	// 1.set
+	return nums.size() > unordered_set<int>(nums.begin(), nums.end()).size();
+
+	// 2.hash
 	unordered_map<int, int> hash;
 	for (auto iter : nums)
 	{
