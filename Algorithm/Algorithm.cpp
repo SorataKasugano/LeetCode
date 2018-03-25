3edab765cf82a8605982af7dad5501aa6de621ff
Sorata Kasugano <884244164@qq.com>
2018-03-25 18:17:28 +0800

    191. Number of 1 Bits

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
-#include "FirstUniqueCharacterinaString.h"
+#include "Numberof1Bits.h"
 
 int main()
 {
-	int n = 10;
-	int* dp = new int[n + 1]{ 0,1,2 };
-	cout << dp[1] << dp[2] << endl;
-	string s("loveleetcode");
-	cout << firstUniqChar(s) << endl;
+	std::cout << hammingWeight(2) << std::endl;
 	return 0;
 }
