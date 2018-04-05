5af0b7008716ca205ec98322622e9c67f2e4484a
Sorata Kasugano <884244164@qq.com>
2018-04-05 15:41:47 +0800

    88. Merge Sorted Array

--- Algorithm/MergeSortedArray.h
+++ Algorithm/MergeSortedArray.h
@@ -18,13 +18,13 @@ void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
 	nums1.erase(nums1.begin() + m + n, nums1.end());
 	sort(nums1.begin(), nums1.end());
 	// 2.traverse and insert
-	int i1, i2;
-	for (i1 = i2 = 0;i1 < m + i2 && i2 < n;i1++)
-		if (nums2[i2] < nums1[i1]) nums1.insert(nums1.begin() + i1, nums2[i2++]);
-	if (i2 < n) nums1.insert(nums1.begin() + i1, nums2.begin() + i2, nums2.begin() + n);
-	nums1.erase(nums1.begin() + m + n, nums1.end());
+	//int i1, i2;
+	//for (i1 = i2 = 0;i1 < m + i2 && i2 < n;i1++)
+	//	if (nums2[i2] < nums1[i1]) nums1.insert(nums1.begin() + i1, nums2[i2++]);
+	//if (i2 < n) nums1.insert(nums1.begin() + i1, nums2.begin() + i2, nums2.begin() + n);
+	//nums1.erase(nums1.begin() + m + n, nums1.end());
 	// 3.reverse traverse and replace
-	nums1.erase(nums1.begin() + m + n, nums1.end());
-	int i = m + n - 1, i1 = m - 1, i2 = n - 1;
-	while (i2 > -1) nums1[i--] = i1 > -1 && nums1[i1] > nums2[i2] ? nums1[i1--] : nums2[i2--];
+	//nums1.erase(nums1.begin() + m + n, nums1.end());
+	//int i = m + n - 1, i1 = m - 1, i2 = n - 1;
+	//while (i2 > -1) nums1[i--] = i1 > -1 && nums1[i1] > nums2[i2] ? nums1[i1--] : nums2[i2--];
 }
